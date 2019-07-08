3d577fd0cbb427054f1e02bc5ccdcb43e14db1e1
mohaoyun <haoyun_mo@126.com>
2019-07-08 23:55:51 +0800

    update

--- uiCrosshair.py
+++ uiCrosshair.py
@@ -1,12 +1,14 @@
 # encoding: UTF-8
 import sys,os
+
 import pyqtgraph as pg
 import datetime as dt          
 import numpy as np
 import traceback
 
-from PyQt5 import QtGui, QtCore
+from PyQt5 import QtWidgets, QtGui, QtCore
 from pyqtgraph.Point import Point
+import pandas as pd
 
 ########################################################################
 # 十字光标支持
@@ -18,10 +20,10 @@ class Crosshair(QtCore.QObject):
     signal = QtCore.Signal(type(tuple([])))
     signalInfo = QtCore.Signal(float,float)
     #----------------------------------------------------------------------
-    def __init__(self,parent,master):
+    def __init__(self, parent, master):
         """Constructor"""
-        self.__view = parent
-        self.master = master
+        self.__view = parent        # PlatWidget
+        self.master = master        # KLineWidget
         super(Crosshair, self).__init__()
 
         self.xAxis = 0
@@ -32,23 +34,24 @@ class Crosshair(QtCore.QObject):
         self.yAxises    = [0 for i in range(3)]
         self.leftX      = [0 for i in range(3)]
         self.showHLine  = [False for i in range(3)]
-        self.textPrices = [pg.TextItem('',anchor=(1,1)) for i in range(3)]
-        self.views      = [parent.centralWidget.getItem(i+1,0) for i in range(3)]
+        self.textPrices = [pg.TextItem('', anchor=(1, 1)) for i in range(3)]
+        # 取得 widget上的：主图/成交量/副图指标
+        self.views      = [parent.centralWidget.getItem(i+1, 0) for i in range(3)]
         self.rects      = [self.views[i].sceneBoundingRect() for i in range(3)]
         self.vLines     = [pg.InfiniteLine(angle=90, movable=False) for i in range(3)]
         self.hLines     = [pg.InfiniteLine(angle=0,  movable=False) for i in range(3)]
         
         #mid 在y轴动态跟随最新价显示最新价和最新时间
-        self.__textDate   = pg.TextItem('date',anchor=(1,1))
-        self.__textInfo   = pg.TextItem('lastBarInfo')   
-        self.__textSig    = pg.TextItem('lastSigInfo',anchor=(1,0))   
-        self.__textSubSig = pg.TextItem('lastSubSigInfo',anchor=(1,0))   
-        self.__textVolume = pg.TextItem('lastBarVolume',anchor=(1,0))   
+        self.__textDate   = pg.TextItem('date', anchor=(1, 1))                              # 文本：日期/时间
+        self.__textInfo   = pg.TextItem('lastBarInfo')                                      # 文本：bar信息
+        self.__text_main_indicators    = pg.TextItem('lastIndicatorsInfo', anchor=(1, 0))   # 文本：主图指标
+        self.__text_sub_indicators = pg.TextItem('lastSubIndicatorsInfo', anchor=(1, 0))    # 文本： 副图指标
+        self.__textVolume = pg.TextItem('lastBarVolume', anchor=(1,0))                      # 文本: 成交量
 
         self.__textDate.setZValue(2)
         self.__textInfo.setZValue(2)
-        self.__textSig.setZValue(2)
-        self.__textSubSig.setZValue(2)
+        self.__text_main_indicators.setZValue(2)
+        self.__text_sub_indicators.setZValue(2)
         self.__textVolume.setZValue(2)
         self.__textInfo.border = pg.mkPen(color=(230, 255, 0, 255), width=1.2)
         
@@ -63,52 +66,81 @@ class Crosshair(QtCore.QObject):
             self.views[i].addItem(self.textPrices[i])
         
         self.views[0].addItem(self.__textInfo, ignoreBounds=True)     
-        self.views[0].addItem(self.__textSig, ignoreBounds=True)     
-        self.views[1].addItem(self.__textVolume, ignoreBounds=True)     
-        self.views[2].addItem(self.__textDate, ignoreBounds=True)
-        self.views[2].addItem(self.__textSubSig, ignoreBounds=True)     
+        self.views[0].addItem(self.__text_main_indicators, ignoreBounds=True)
+
+        # 添加成交量的提示信息到 volume view
+        if self.master.display_vol:
+            self.views[1].addItem(self.__textVolume, ignoreBounds=True)
+
+        # 添加 副图指标/日期信息到 副图
+        if self.master.display_sub:
+            self.views[2].addItem(self.__textDate, ignoreBounds=True)
+            self.views[2].addItem(self.__text_sub_indicators, ignoreBounds=True)
+        else:
+            # 没有启用副图，日期信息放在主图
+            self.views[0].addItem(self.__textDate, ignoreBounds=True)
+
         self.proxy = pg.SignalProxy(self.__view.scene().sigMouseMoved, rateLimit=360, slot=self.__mouseMoved)        
         # 跨线程刷新界面支持
         self.signal.connect(self.update)
         self.signalInfo.connect(self.plotInfo)
 
     #----------------------------------------------------------------------
-    def update(self,pos):
+    def update(self, pos):
         """刷新界面显示"""
-        xAxis,yAxis = pos
-        xAxis,yAxis = (self.xAxis,self.yAxis) if xAxis is None else (xAxis,yAxis)
-        self.moveTo(xAxis,yAxis)
+        try:
+            xAxis, yAxis = pos
+            xAxis, yAxis = (self.xAxis, self.yAxis) if xAxis is None else (xAxis, yAxis)
+            self.moveTo(xAxis, yAxis)
+        except Exception as ex:
+            print(u'Crosshair.update() exception:{},trace:{}'.format(str(ex), traceback.format_exc()))
         
     #----------------------------------------------------------------------
     def __mouseMoved(self,evt):
         """鼠标移动回调"""
-        pos = evt[0]  
-        self.rects = [self.views[i].sceneBoundingRect() for i in range(3)]
-        for i in range(3):
-            self.showHLine[i] = False
-            if self.rects[i].contains(pos):
-                mousePoint = self.views[i].vb.mapSceneToView(pos)
-                xAxis = mousePoint.x()
-                yAxis = mousePoint.y()    
-                self.yAxises[i] = yAxis
-                self.showHLine[i] = True
-                self.moveTo(xAxis,yAxis)
+        try:
+            pos = evt[0]
+            self.rects = [self.views[i].sceneBoundingRect() for i in range(3)]
+            for i in range(3):
+                self.showHLine[i] = False
+                if i == 1 and not self.master.display_vol:
+                    continue
+
+                if i == 2 and not self.master.display_sub:
+                    continue
+
+                if self.rects[i].contains(pos):
+                    mousePoint = self.views[i].vb.mapSceneToView(pos)
+                    xAxis = mousePoint.x()
+                    yAxis = mousePoint.y()
+                    self.yAxises[i] = yAxis
+                    self.showHLine[i] = True
+                    self.moveTo(xAxis, yAxis)
+        except Exception as ex:
+            print(u'_mouseMove() exception:{},trace:{}'.format(str(ex), traceback.format_exc()))
 
     #----------------------------------------------------------------------
-    def moveTo(self,xAxis,yAxis):
-        xAxis,yAxis = (self.xAxis,self.yAxis) if xAxis is None else (int(xAxis),yAxis)
-        self.rects  = [self.views[i].sceneBoundingRect() for i in range(3)]
-        if not xAxis or not yAxis:
-            return
-        self.xAxis = xAxis
-        self.yAxis = yAxis
-        self.vhLinesSetXY(xAxis,yAxis)
-        self.plotInfo(xAxis,yAxis)
-        if hasattr(self.master, 'volume'):
-            self.master.volume.update()
-
+    def moveTo(self, xAxis, yAxis):
+        """
+        移动
+        :param xAxis:
+        :param yAxis:
+        :return:
+        """
+        try:
+            xAxis,yAxis = (self.xAxis,self.yAxis) if xAxis is None else (int(xAxis),yAxis)
+            self.rects  = [self.views[i].sceneBoundingRect() for i in range(3)]
+            if not xAxis or not yAxis:
+                return
+            self.xAxis = xAxis
+            self.yAxis = yAxis
+            self.vhLinesSetXY(xAxis,yAxis)
+            self.plotInfo(xAxis,yAxis)
+            self.master.pi_volume.update()
+        except Exception as ex:
+            print(u'_mouseMove() exception:{},trace:{}'.format(str(ex), traceback.format_exc()))
     #----------------------------------------------------------------------
-    def vhLinesSetXY(self,xAxis,yAxis):
+    def vhLinesSetXY(self,xAxis, yAxis):
         """水平和竖线位置设置"""
         for i in range(3):
             self.vLines[i].setPos(xAxis)
@@ -119,32 +151,44 @@ class Crosshair(QtCore.QObject):
                 self.hLines[i].hide()
 
     #----------------------------------------------------------------------
-    def plotInfo(self,xAxis,yAxis):        
+    def plotInfo(self, xAxis, yAxis):
         """
         被嵌入的plotWidget在需要的时候通过调用此方法显示K线信息
         """
-        if self.datas is None:
+        if self.datas is None or xAxis >= len(self.datas):
             return
+
+        tickDatetime = None
+        openPrice = 0
+        closePrice = 0
+        highPrice = 0
+        lowPrice = 0
+        preClosePrice = 0
+        volume = 0
+        openInterest = 0
+
         try:
             # 获取K线数据
             data            = self.datas[xAxis]
             lastdata        = self.datas[xAxis-1]
-            tickDatetime    = data['datetime']
+            tickDatetime    = pd.to_datetime(data['datetime'])
             openPrice       = data['open']
             closePrice      = data['close']
             lowPrice        = data['low']
             highPrice       = data['high']
             volume          = int(data['volume'])
-            openInterest    = int(data['openInterest'])
+            # openInterest    = int(data['openInterest'])
             preClosePrice   = lastdata['close']
-            tradePrice      = abs(self.master.listSig[xAxis])
-        except Exception as e:
+
+        except Exception as ex:
+            print(u'exception:{},trace:{}'.format(str(ex), traceback.format_exc()))
             return
-            
-        if(isinstance(tickDatetime,dt.datetime)):
-            datetimeText = dt.datetime.strftime(tickDatetime,'%Y-%m-%d %H:%M:%S')
-            dateText     = dt.datetime.strftime(tickDatetime,'%Y-%m-%d')
-            timeText     = dt.datetime.strftime(tickDatetime,'%H:%M:%S')
+
+
+        if( isinstance(tickDatetime, dt.datetime)):
+            datetimeText = dt.datetime.strftime(tickDatetime, '%Y-%m-%d %H:%M:%S')
+            dateText     = dt.datetime.strftime(tickDatetime, '%Y-%m-%d')
+            timeText     = dt.datetime.strftime(tickDatetime, '%H:%M:%S')
         else:
             datetimeText = ""
             dateText     = ""
@@ -152,23 +196,22 @@ class Crosshair(QtCore.QObject):
 
         # 显示所有的主图技术指标
         html = u'<div style="text-align: right">'
-        for sig in self.master.sigData:
-            val = self.master.sigData[sig][xAxis]
-            col = self.master.sigColor[sig]
-            html+= u'<span style="color: %s;  font-size: 18px;">&nbsp;&nbsp;%s：%.2f</span>' %(col,sig,val)
-        html+=u'</div>' 
-        self.__textSig.setHtml(html)
+        for indicator in self.master.main_indicator_data:
+            val   = self.master.main_indicator_data[indicator][xAxis]
+            col   = self.master.main_indicator_colors[indicator]
+            html += u'<span style="color: %s;  font-size: 18px;">&nbsp;&nbsp;%s：%.2f</span>' %(col,indicator,val)
+        html += u'</div>'
+        self.__text_main_indicators.setHtml(html)
 
         # 显示所有的主图技术指标
         html = u'<div style="text-align: right">'
-        for sig in self.master.subSigData:
-            val = self.master.subSigData[sig][xAxis]
-            col = self.master.subSigColor[sig]
-            html+= u'<span style="color: %s;  font-size: 18px;">&nbsp;&nbsp;%s：%.2f</span>' %(col,sig,val)
-        html+=u'</div>' 
-        self.__textSubSig.setHtml(html)
+        for indicator in self.master.sub_indicator_data:
+            val   = self.master.sub_indicator_data[indicator][xAxis]
+            col   = self.master.sub_indicator_colors[indicator]
+            html += u'<span style="color: %s;  font-size: 18px;">&nbsp;&nbsp;%s：%.2f</span>' %(col,indicator,val)
+        html += u'</div>'
+        self.__text_sub_indicators.setHtml(html)
 
-        
         # 和上一个收盘价比较，决定K线信息的字符颜色
         cOpen     = 'red' if openPrice  > preClosePrice else 'green'
         cClose    = 'red' if closePrice > preClosePrice else 'green'
@@ -188,34 +231,42 @@ class Crosshair(QtCore.QObject):
                                 <span style="color: %s;     font-size: 16px;">(收) %.3f</span><br>\
                                 <span style="color: white;  font-size: 16px;">成交量</span><br>\
                                 <span style="color: yellow; font-size: 16px;">(量) %d</span><br>\
-                                <span style="color: white;  font-size: 16px;">成交价</span><br>\
-                                <span style="color: yellow; font-size: 16px;">(价) %.3f</span><br>\
                             </div>'\
                                 % (dateText,timeText,cOpen,openPrice,cHigh,highPrice,\
-                                    cLow,lowPrice,cClose,closePrice,volume,tradePrice))             
+                                    cLow,lowPrice,cClose,closePrice,volume))
         self.__textDate.setHtml(
                             '<div style="text-align: center">\
-                                <span style="color: yellow; font-size: 18px;">%s</span>\
+                                <span style="color: yellow; font-size: 12px;">%s</span>\
                             </div>'\
                                 % (datetimeText))   
 
         self.__textVolume.setHtml(
                             '<div style="text-align: right">\
-                                <span style="color: white; font-size: 18px;">VOL : %.3f</span>\
+                                <span style="color: white; font-size: 12px;">VOL : %.3f</span>\
                             </div>'\
                                 % (volume))   
         # 坐标轴宽度
         rightAxisWidth = self.views[0].getAxis('right').width()
         bottomAxisHeight = self.views[2].getAxis('bottom').height()           
-        offset = QtCore.QPointF(rightAxisWidth,bottomAxisHeight)
+        offset = QtCore.QPointF(rightAxisWidth, bottomAxisHeight)
 
         # 各个顶点
-        tl = [self.views[i].vb.mapSceneToView(self.rects[i].topLeft()) for i in range(3)]
-        br = [self.views[i].vb.mapSceneToView(self.rects[i].bottomRight()-offset) for i in range(3)]
+        t1 = [None, None, None]
+        br = [None, None, None]
+        t1[0] = self.views[0].vb.mapSceneToView(self.rects[0].topLeft())
+        br[0] = self.views[0].vb.mapSceneToView(self.rects[0].bottomRight() - offset)
+
+        if self.master.display_vol:
+            t1[1] = self.views[1].vb.mapSceneToView(self.rects[1].topLeft())
+            br[1] = self.views[1].vb.mapSceneToView(self.rects[1].bottomRight() - offset)
+        if self.master.display_sub:
+            t1[2] = self.views[2].vb.mapSceneToView(self.rects[2].topLeft())
+            br[2] = self.views[2].vb.mapSceneToView(self.rects[2].bottomRight() - offset)
+
 
         # 显示价格
         for i in range(3):
-            if self.showHLine[i]:
+            if self.showHLine[i] and t1[i] is not None and br[i] is not None:
                 self.textPrices[i].setHtml(
                         '<div style="text-align: right">\
                              <span style="color: yellow; font-size: 18px;">\
@@ -230,11 +281,18 @@ class Crosshair(QtCore.QObject):
 
         
         # 设置坐标
-        self.__textInfo.setPos(tl[0])           
-        self.__textSig.setPos(br[0].x(),tl[0].y())           
-        self.__textSubSig.setPos(br[2].x(),tl[2].y())           
-        self.__textVolume.setPos(br[1].x(),tl[1].y())           
+        self.__textInfo.setPos(t1[0])
+        self.__text_main_indicators.setPos(br[0].x(), t1[0].y())
+        if self.master.display_sub:
+            self.__text_sub_indicators.setPos(br[2].x(), t1[2].y())
+        if self.master.display_vol:
+            self.__textVolume.setPos(br[1].x(),t1[1].y())
 
         # 修改对称方式防止遮挡
         self.__textDate.anchor = Point((1,1)) if xAxis > self.master.index else Point((0,1))
-        self.__textDate.setPos(xAxis,br[2].y())
+        if br[2] is not None:
+            self.__textDate.setPos(xAxis, br[2].y())
+        elif br[1] is not None:
+            self.__textDate.setPos(xAxis, br[1].y())
+        else:
+            self.__textDate.setPos(xAxis, br[0].y())
